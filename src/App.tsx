eb43d778945de5accdc761d90b6d074692035337
Maksym Kostiv <rafik9202@gmail.com>
2025-04-08 13:06:13 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './components/Header/Header';
 import { TodoList } from './components/TodoList/TodoList';
 import { ErrorMessage, Filterby, Todo } from './types/Todo';
